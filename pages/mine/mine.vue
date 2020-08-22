2e1e76a00cf01263f5397b75ac4f3c613dd07333
柿子不如橙子 <601326173@qq.com>
2020-08-22 14:19:29 +0800

    解决收藏分页问题

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -1,121 +1,102 @@
 <template>
-	<view class="content" style="height: 100%; overflow: hidden;">
-		<view class="contentTop">
-			<image src="../../static/images/mineBack.png" class="backImg"></image>
-			<!-- 用户信息 -->
-			<view class="usermes">
-				<image class="userAva" :src="avatarUrl"></image>
-				<view class="userR">
-					<view class="userName">{{ nickName }}</view>
-					<!-- <view class="logout">退出登录</view> -->
+	<view class="content">
+		<view  style="position: fixed; width: 100%; top: 0; z-index: 400;">
+			<view class="contentTop">
+				<image src="../../static/images/mineBack.png" class="backImg"></image>
+				<!-- 用户信息 -->
+				<view class="usermes">
+					<image class="userAva" :src="avatarUrl"></image>
+					<view class="userR">
+						<view class="userName">{{ nickName }}</view>
+						<!-- <view class="logout">退出登录</view> -->
+					</view>
 				</view>
 			</view>
+			
+			<!-- 我的收藏 -->
+			<view class="myCollection">
+				<view class="phone"><image class="phoneImg" src="../../static/images/phone.png" mode=""></image></view>
+				<view>我的收藏</view>
+			</view>
 		</view>
-		
-		<!-- 我的收藏 -->
-		<view class="myCollection" style="padding-bottom: 200rpx;">
-			<view class="phone"><image class="phoneImg" src="../../static/images/phone.png" mode=""></image></view>
-			
-			<view>我的收藏</view>
-			<scroll-view scroll-y="true" class="list" >
-			
-				<mescroll-body  ref="mescrollRef" @init="mescrollInit" @down="downCallback" @up="upCallback" :down="downOption"  :up="upOption">
+		<view style="margin-top: 60%; padding: 0 24rpx;">
+				<mescroll-body  ref="mescrollRef" @init="mescrollInit" @down="downCallback" @up="upCallback" :down="downOption" :up="upOption">
 					<view class="contentItem" v-for="(item, index) in tipList" :key="index">
 						<view class="left">
 							<image :src="item.main_image" mode="">
 								<view class="imgTip">
-									<view  v-if="item.type==1">
-										游记
-									</view>
-									<view  v-if="item.type==2">
-										攻略
-									</view>
+									<view v-if="item.type == 1">游记</view>
+									<view v-if="item.type == 2">攻略</view>
 								</view>
 							</image>
 						</view>
-						<view class="right" @click="onPageJump" :id= "item.article_id">
+						<view class="right" @click="onPageJump" :id="item.article_id">
 							<view class="title">
-								<view class="tips" v-if="item.type==1">
-									游记
-								</view>
-								<view class="tips" v-if="item.type==2">
-									攻略
-								</view>
-								| 
-								<view class="titleText">
-									{{item.title}}
-								</view>
+								<view class="tips" v-if="item.type == 1">游记</view>
+								<view class="tips" v-if="item.type == 2">攻略</view>
+								<view class="titleText">{{ item.title }}</view>
 							</view>
 							<view class="content">
-								<rich-text class="richText" :nodes="item.content "></rich-text>
-								<!-- {{item.title}} -->
+								<rich-text class="richText" :nodes="item.content"></rich-text>
 							</view>
 							<view class="position">
 								<image src="../../static/images/Icon／Map.svg" mode="aspectFill"></image>
-								<view>{{item.location}}</view>
+								<view>{{ item.location }}</view>
 							</view>
 						</view>
 					</view>
 				</mescroll-body>
-				<!-- <view class="noContentItem" v-show="tipList == null">
-					<image src="../../static/images/wenjianjia.png" mode=""></image>
-					<view class="tipText">您的收藏夹空空如也~</view>
-				</view> -->
-			</scroll-view>
 		</view>
 	</view>
-	
 </template>
 
 <script>
 import { mapState, mapMutations } from 'vuex';
 import httpType from '../../httpType.js';
-import MescrollMixin from "@/components/mescroll-uni/mescroll-mixins.js";
+import MescrollMixin from '@/components/mescroll-uni/mescroll-mixins.js';
 export default {
 	data() {
 		return {
 			nickName: uni.getStorageSync('nickName'),
-			avatarUrl:"",
-			tipList:[]
+			avatarUrl: '',
+			tipList: []
 		};
 	},
 	mixins: [MescrollMixin],
 	computed: mapState(['forcedLogin', 'hasLogin', 'phone']),
-	
-	
+
 	onLoad() {
-		this.getUserMsg()
+		this.getUserMsg();
 	},
-	
+
 	methods: {
-		getUserMsg(){
-			var that = this
-			
+		getUserMsg() {
+			var that = this;
+
 			uni.login({
-			  provider: 'baidu',
-			  success: function(loginRes) {
-			    console.log(loginRes.authResult);
-			    // 获取用户信息
-			    uni.getUserInfo({
-			      provider: 'baidu',
-			      success:  function(infoRes) {
-			        console.log('用户昵称为：' + infoRes.userInfo.nickName);
-					var infoRes = infoRes.userInfo
-					console.log(infoRes.nickName)
-					that.nickName = infoRes.nickName
-					that.avatarUrl = infoRes.avatarUrl
-					uni.setStorageSync('nickName',infoRes.nickName)
-					uni.setStorageSync('avatarUrl',infoRes.avatarUrl)
-			      }
-			    })
-				
-			  }
+				provider: 'baidu',
+				success: function(loginRes) {
+					console.log(loginRes.authResult);
+					// 获取用户信息
+					uni.getUserInfo({
+						provider: 'baidu',
+						success: function(infoRes) {
+							console.log('用户昵称为：' + infoRes.userInfo.nickName);
+							var infoRes = infoRes.userInfo;
+							console.log(infoRes.nickName);
+							that.nickName = infoRes.nickName;
+							that.avatarUrl = infoRes.avatarUrl;
+							uni.setStorageSync('nickName', infoRes.nickName);
+							uni.setStorageSync('avatarUrl', infoRes.avatarUrl);
+						}
+					});
+				}
 			});
-			
+
 			uni.getStorage({
-				key:'Authorization',
-				success:function(res){
-					console.log("token===>",res.data)
+				key: 'Authorization',
+				success: function(res) {
+					console.log('token===>', res.data);
 					// uni.request({
 					// 	url:'http://121.40.30.19/user/info',
 					// 	header:{
@@ -127,74 +108,69 @@ export default {
 					// })
 				}
 			}),
-			
-			uni.request({
-				url:"http://121.40.30.19/user/info",
-				header:{
-					'Authorization':uni.getStorageSync('Authorization')
-				},
-				method:'get',
-				success:function(res){
-					console.log('个人信息=',res.data)
-					if (res.data.code != 0) {
-						// debugger
-						uni.showModal({
-							title: '提示',
-							content: '您好，请先登录',
-							showCancel: false,
-							success: function(res) {
-								if (res.confirm) {
-									uni.redirectTo({
-										url: '../login/login'
-									})
+				uni.request({
+					url: 'http://121.40.30.19/user/info',
+					header: {
+						Authorization: uni.getStorageSync('Authorization')
+					},
+					method: 'get',
+					success: function(res) {
+						console.log('个人信息=', res.data);
+						if (res.data.code != 0) {
+							// debugger
+							uni.showModal({
+								title: '提示',
+								content: '您好，请先登录',
+								showCancel: false,
+								success: function(res) {
+									if (res.confirm) {
+										uni.redirectTo({
+											url: '../login/login'
+										});
+									}
 								}
-							}
-						})
-						return
+							});
+							return;
+						}
+						uni.setStorageSync('mobile', res.data);
+						console.log('存储信息', res.data);
 					}
-					uni.setStorageSync('mobile',res.data)
-					console.log('存储信息',res.data)
-					
-					
-				}
-			}),
-			uni.request({
-				url:'http://121.40.30.19/user/favorite/list',
-				data:{
-					'count':5,
-					'page':1
-				},
-				header:{
-					'Authorization':uni.getStorageSync('Authorization')
-				},
-				method:'get',
-				success:function(res){
-					console.log('收藏列表',res.data)
-					that.tipList = res.data.data.list
-					console.log('1111111',that.tipList)
-				}
-			})
-			
+				}),
+				uni.request({
+					url: 'http://121.40.30.19/user/favorite/list',
+					data: {
+						count: 5,
+						page: 1
+					},
+					header: {
+						Authorization: uni.getStorageSync('Authorization')
+					},
+					method: 'get',
+					success: function(res) {
+						console.log('收藏列表', res.data);
+						that.tipList = res.data.data.list;
+						console.log('1111111', that.tipList);
+					}
+				});
 		},
 		// 跳转文章详情
 		onPageJump(e) {
-			console.log(e)
-			let id = e.currentTarget.id
+			console.log(e);
+			let id = e.currentTarget.id;
 			// debugger
 			// return
 			uni.navigateTo({
-				url: "/pages/contentdetail/contentdetail?article_id="+id
-			})
+				url: '/pages/contentdetail/contentdetail?article_id=' + id
+			});
 		},
-		
+
 		/*下拉刷新的回调, 有三种处理方式:*/
-		downCallback(){
-			
+		downCallback() {
 			// 第2种: 下拉刷新和上拉加载调同样的接口, 那么不用第1种方式, 直接mescroll.resetUpScroll()即可
 			this.mescroll.resetUpScroll(); // 重置列表为第一页 (自动执行 page.num=1, 再触发upCallback方法 )
 			// 第3种: 下拉刷新什么也不处理, 可直接调用或者延时一会调用 mescroll.endSuccess() 结束即可
 			// this.mescroll.endSuccess()
-			
+
 			// 此处仍可以继续写其他接口请求...
 			// 调用其他方法...
 		},
@@ -204,71 +180,63 @@ export default {
 			let pageNum = page.num; // 页码, 默认从1开始
 			let pageSize = page.size; // 页长, 默认每页10条
 			uni.request({
-				url: 'http://121.40.30.19/user/favorite/list?page='+pageNum+'&count='+pageSize,
-				header:{
-					'Authorization':uni.getStorageSync('Authorization')
+				url: 'http://121.40.30.19/user/favorite/list?page=' + pageNum + '&count=' + pageSize,
+				header: {
+					Authorization: uni.getStorageSync('Authorization')
 				},
-				success: (data) => {
-					console.log('data',data)
+				success: data => {
+					console.log('data', data);
 					// 接口返回的当前页数据列表 (数组)
-					let curPageData = data.data.data.list; 
-					console.log('curPageData',curPageData)
+					let curPageData = data.data.data.list;
+					console.log('curPageData', curPageData);
 					// 接口返回的当前页数据长度 (如列表有26个数据,当前页返回8个,则curPageLen=8)
-					let curPageLen = curPageData.length; 
-					console.log('curPageLen',curPageLen)
+					let curPageLen = curPageData.length;
+					console.log('curPageLen', curPageLen);
 					// 接口返回的总页数 (如列表有26个数据,每页10条,共3页; 则totalPage=3)
-					// let totalPage = data.data.data.list; 
+					// let totalPage = data.data.data.list;
 					// 接口返回的总数据量(如列表有26个数据,每页10条,共3页; 则totalSize=26)
-					let totalSize = data.data.data.total; 
-					console.log('totalSize',totalSize)
+					let totalSize = data.data.data.total;
+					console.log('totalSize', totalSize);
 					// 接口返回的是否有下一页 (true/false)
-					// let hasNext = data.data.data.list; 
-					
+					// let hasNext = data.data.data.list;
+
 					//设置列表数据
-					if(page.num == 1) this.tipList = []; //如果是第一页需手动置空列表
+					if (page.num == 1) this.tipList = []; //如果是第一页需手动置空列表
 					this.tipList = this.tipList.concat(curPageData); //追加新数据
-					console.log('tipList',this.tipList)
+					console.log('tipList', this.tipList);
 					// 请求成功,隐藏加载状态
 					//方法一(推荐): 后台接口有返回列表的总页数 totalPage
-					// this.mescroll.endByPage(curPageLen, totalPage); 
-					
+					// this.mescroll.endByPage(curPageLen, totalPage);
+
 					//方法二(推荐): 后台接口有返回列表的总数据量 totalSize
-					this.mescroll.endBySize(curPageLen, totalSize); 
-					
+					this.mescroll.endBySize(curPageLen, totalSize);
+
 					//方法三(推荐): 您有其他方式知道是否有下一页 hasNext
-					//this.mescroll.endSuccess(curPageLen, hasNext); 
-					
+					//this.mescroll.endSuccess(curPageLen, hasNext);
+
 					//方法四 (不推荐),会存在一个小问题:比如列表共有20条数据,每页加载10条,共2页.
 					//如果只根据当前页的数据个数判断,则需翻到第三页才会知道无更多数据
 					//如果传了hasNext,则翻到第二页即可显示无更多数据.
 					//this.mescroll.endSuccess(curPageLen);
-					
+
 					// 如果数据较复杂,可等到渲染完成之后再隐藏下拉加载状态: 如
 					// 建议使用setTimeout,因为this.$nextTick某些情况某些机型不触发
-					setTimeout(()=>{
-						this.mescroll.endSuccess(curPageLen)
-					},20)
-					
-					
 				},
 				fail: () => {
 					//  请求失败,隐藏加载状态
-					this.mescroll.endErr()
+					this.mescroll.endErr();
 				}
-			})
-			
+			});
+
 			// 此处仍可以继续写其他接口请求...
 			// 调用其他方法...
 		},
-		...mapMutations(['login']),
-		
-	},
-	
+		...mapMutations(['login'])
+	}
 };
 </script>
 
 <style lang="scss" scoped>
-
 .backImg {
 	position: absolute;
 	height: 440rpx;
@@ -281,19 +249,22 @@ export default {
 	padding-top: 174rpx;
 	height: 130rpx;
 	display: flex;
-	align-items: center;
+	height:340rpx ;
+	// align-items: center;
+	
 }
 .userAva {
 	margin-left: 15px;
 	width: 130rpx;
 	height: 130rpx;
+	
 	// border: 1px #333333 solid;
 	border-radius: 50%;
 	margin-top: 86rpx;
 }
 .userR {
 	margin-left: 32rpx;
-	margin-top: 104rpx;
+	margin-top: 24rpx;
 }
 .userName {
 	font-size: 36rpx;
@@ -317,47 +288,44 @@ export default {
 	border-radius: 40rpx 40rpx 0rpx 0rpx;
 	background-color: #fff;
 	color: #303133;
-	height: 1106rpx;
+	width: 100%;
 	font-size: 40rpx;
 	font-weight: 500;
 	padding-left: 32rpx;
 	padding-top: 52rpx;
-	position: relative;
-	top: 190rpx;
-	z-index: 21;
+	position: absolute;
+	// top: 190rpx;
 	.phone {
 		height: 120rpx;
 		width: 120rpx;
 		border-radius: 50%;
 		position: absolute;
 		top: -60rpx;
-		right:32rpx;
+		right: 32rpx;
 	}
 	.phone .phoneImg {
 		width: 120rpx;
 		height: 120rpx;
 	}
 }
-.list{
-	height: 100%;
-}
-.noContentItem{
+
+.noContentItem {
 	// height: 600rpx;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-	image{
+	image {
 		width: 154rpx;
 		height: 122rpx;
 		position: fi;
 	}
-	.tipText{
-		font-size:28rpx;
-		font-family:PingFangSC-Regular,PingFang SC;
-		font-weight:400;
-		color:rgba(144,147,153,1);
-		line-height:30rpx;
+	.tipText {
+		font-size: 28rpx;
+		font-family: PingFangSC-Regular, PingFang SC;
+		font-weight: 400;
+		color: rgba(144, 147, 153, 1);
+		line-height: 30rpx;
 		margin-top: 40rpx;
 	}
 }
@@ -370,15 +338,15 @@ export default {
 }
 
 .contentItem {
-	width:694rpx;
-	height:230rpx;
+	width: 694rpx;
+	height: 230rpx;
 	margin: 28rpx;
 	margin-left: 0;
 	border-radius: 8px;
 	box-shadow: 4rpx 4rpx 20rpx 0rpx rgba(0, 0, 0, 0.08);
 	display: flex;
 	overflow: hidden;
-	text-overflow:ellipsis;
+	text-overflow: ellipsis;
 	white-space: nowrap;
 	.left {
 		position: relative;
@@ -393,7 +361,7 @@ export default {
 			width: 96rpx;
 			height: 44rpx;
 			text-align: center;
-			background-color: rgba(0,0,0,0.6);
+			background-color: rgba(0, 0, 0, 0.6);
 			border-radius: 16rpx 0 16rpx 0;
 		}
 		image {
@@ -401,8 +369,8 @@ export default {
 			width: 208rpx;
 			height: 246rpx;
 			margin-right: 20rpx;
-			box-shadow:2px 2px 10px 0px rgba(0,0,0,0.08);
-			border-radius:16rpx 0 0 16rpx;
+			box-shadow: 2px 2px 10px 0px rgba(0, 0, 0, 0.08);
+			border-radius: 16rpx 0 0 16rpx;
 		}
 	}
 
@@ -418,29 +386,27 @@ export default {
 		height: 32rpx;
 		font-size: 32rpx;
 		font-weight: 500;
-		color: rgba(48,49,51,1);
-		font-family:PingFangSC-Medium,PingFang SC;
-		line-height:32rpx;
+		color: rgba(48, 49, 51, 1);
+		font-family: PingFangSC-Medium, PingFang SC;
+		line-height: 32rpx;
 		display: flex;
-		
 	}
-	.tips{
+	.tips {
 		margin-right: 10rpx;
 	}
-	.titleText{
+	.titleText {
 		margin-left: 10rpx;
 		overflow: hidden;
-		text-overflow:ellipsis;
+		text-overflow: ellipsis;
 		white-space: nowrap;
-
 	}
-	.richText{
-		width:448rpx;
-		height:70rpx;
+	.richText {
+		width: 448rpx;
+		height: 70rpx;
 		font-size: 28rpx;
 		font-weight: 400;
-		color:rgba(96,98,102,1);
-		line-height:38rpx;
+		color: rgba(96, 98, 102, 1);
+		line-height: 38rpx;
 		margin-top: 20rpx;
 		display: -webkit-box;
 		overflow: hidden;
@@ -449,7 +415,6 @@ export default {
 		white-space: normal !important;
 		-webkit-line-clamp: 2;
 		-webkit-box-orient: vertical;
-		
 	}
 	.right .position {
 		display: flex;
@@ -464,12 +429,12 @@ export default {
 		view {
 			width: 176rpx;
 			font-size: 22rpx;
-			font-family:PingFangSC-Regular,PingFang SC;
-			font-weight:400;
-			color:rgba(0,145,255,1);
-			line-height:22rpx;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			color: rgba(0, 145, 255, 1);
+			line-height: 22rpx;
 			overflow: hidden;
-			text-overflow:ellipsis;
+			text-overflow: ellipsis;
 			white-space: nowrap;
 		}
 	}
