cbbb368e6b0219de746c9ea20bc3b08a35a6d1df
柿子不如橙子 <601326173@qq.com>
2020-09-02 08:56:24 +0800

    解决冲突

--- main.js
+++ main.js
@@ -6,11 +6,7 @@ import store from './store'
 Vue.prototype.$store = store
 import uView from "uview-ui";
 Vue.use(uView);
-<<<<<<< HEAD
-Vue.prototype.globalUrl = 'http://10.0.2.11:8199'
-=======
 Vue.prototype.globalUrl = 'https://devapi.lingtuyang.cn'
->>>>>>> feature/song
 // import http from './api/api.js'
 // import MescrollUni from "@/components/mescroll-uni/mescroll-uni.vue"
 // Vue.component('mescroll-uni', MescrollUni)	
