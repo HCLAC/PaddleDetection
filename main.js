348ecc77b443fc5c28c01d242c095f0556bdf68b
柿子不如橙子 <601326173@qq.com>
2020-09-02 13:48:09 +0800

    添加页面跳转链接

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 //引入vuex
+import globalUrl from './global.js'
 import store from './store'
 //把vuex定义成全局组件
 Vue.prototype.$store = store
 import uView from "uview-ui";
 Vue.use(uView);
-Vue.prototype.globalUrl = 'https://devapi.lingtuyang.cn'
+Vue.prototype.globalUrl = globalUrl
 // import http from './api/api.js'
 // import MescrollUni from "@/components/mescroll-uni/mescroll-uni.vue"
 // Vue.component('mescroll-uni', MescrollUni)	
