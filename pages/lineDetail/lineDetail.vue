45645041ce9f6c7868722848eee3fdb4ce39e5a3
柿子不如橙子 <601326173@qq.com>
2020-09-04 18:13:06 +0800

    解决ios热门目的地不显示我呢提‘

--- pages/lineDetail/lineDetail.vue
+++ pages/lineDetail/lineDetail.vue
@@ -214,7 +214,7 @@
 						<image v-show="lineContent.fav" class="favBtn" src="../../static/images/fav-actived.svg"></image>
 						<view class="favNum">{{ lineContent.fav_count }}</view>
 					</view> -->
-					<view class="share"><image src="../../static/images/fenxiang.svg"></image></view>
+					<view class="share" @click="showShare"><image src="../../static/images/fenxiang.svg"></image></view>
 					<view class=""><view class="loginButton" v-if="!hasLogin" @click="login">登录</view></view>
 				</view>
 			</view>
@@ -255,6 +255,9 @@ export default {
 		}
 	},
 	methods: {
+		showShare(){
+			uni.showShareMenu({});
+		},
 		tabChange(index) {
 			this.tabCurrent = index;
 		},
