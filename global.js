cbfb340d2dae1a56981e68a1cfe75e0d90f5cd51
ZZZUTER <805877178@qq.com>
2020-09-22 18:17:00 +0800

    文章详情接口测试

--- global.js
+++ global.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-// export default 'https://devapi.lingtuyang.cn/v2'
-export default 'http://192.168.110.125:8199/v2'
-=======
 export default 'https://devapi.lingtuyang.cn/v2'
 // export default 'http://192.168.110.69:8199'
->>>>>>> 651c1278cfbc2d1dec8bdbbc6ee948ddbafee2cc
