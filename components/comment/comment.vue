ca69aa17de99f69e77a06ff362efb7e44e69caae
ZZZUTER <805877178@qq.com>
2020-11-12 09:45:10 +0800

    优化键盘ui效果

--- components/comment/comment.vue
+++ components/comment/comment.vue
@@ -2,21 +2,20 @@
 	<view @touchmove.stop.prevent="" class="mask" :class="maskState===0 ? 'none' : maskState===1 ? 'show' : ''" @click="toggleMask">
 		<view class="mask-content"  @click.stop.prevent="stopPrevent">
 			<view class="mask-content-topbar">
-				<view class="left" >
-					<view class="mask-content-input">
-						<textarea class="textarea"
-							v-model="content"
-							:placeholder="placeholder"
-							:cursor-spacing = "100"
-							:show-confirm-bar = "false"
-							:focus="focus"
-							:auto-height="autoHeight"
-							@input="inputValue"
-							maxlength="140"></textarea>
-					</view>
+				<view class="mask-content-input">
+					<textarea class="textarea"
+						v-model="content"
+						:placeholder="placeholder"
+						:cursor-spacing = "30"
+						:show-confirm-bar = "false"
+						:focus="focus"
+						:auto-height="autoHeight"
+						@input="inputValue"
+						maxlength="140">
+					</textarea>
 				</view>
-				
 				<view class="right" @click="pubComment">发送</view>
+				
 			</view>
 			
 		</view>
@@ -91,55 +90,64 @@
 		width: 100%;
 		background: rgba(0,0,0,0);
 		z-index: 9995;
-		transition: .3s;
-		-webkit-transition: .3s; 
+		transition: .1s;
+		-webkit-transition: .1s; 
 		.mask-content{
 			width: 100%;
 			background: #FFFFFF;
 			transform: translateY(100%);
-			transition: .3s;//底部弹出的持续时间
-			-webkit-transition: .3s;//底部弹出的持续时间 
+			transition: .1s;//底部弹出的持续时间
+			-webkit-transition: .1s;//底部弹出的持续时间 
 			// overflow-y:scroll;
 			display: flex;
-			flex-direction: column;
+			// flex-direction: column;
 			.mask-content-topbar{
 				display: flex;
 				align-items: center;
 				// flex-direction: row;
-				justify-content: space-between;
+				// justify-content: space-between;
 				padding: 16rpx 32rpx 14rpx;
 				font-size: 28rpx;
-				.left{
-					// padding: 10upx 0upx;
-					color: $font-color-base;
+				// .left{
+				// 	// padding: 10upx 0upx;
+				// 	color: $font-color-base;
+				// }
+				.mask-content-input{
+					// width: 718upx;//如果textarea的宽带有问题可以把width改为100%试试
+					// width: 100%;
+					// padding: 10upx 16upx 20upx;
+					.textarea {
+						width: 590rpx;
+						height: 28rpx;
+						background: #F8F8F8;
+						border-radius: 4px;
+						padding: 16rpx 16rpx;
+						
+						margin-right: 16rpx;
+						font-size: 28rpx;
+						font-family: PingFangSC-Regular, PingFang SC;
+						font-weight: 400;
+						color: #C9CAD1;
+						// height: 100px;
+						// width: 686upx;//如果textarea的宽带有问题可以把width改为100%试试
+						// width: 100%;
+						// padding: 20rpx 16rpx;
+						// border:2upx solid #d5d5d6;
+						// border-radius: 8upx;
+					}
 				}
 				.right{
-					// padding: 10upx 100upx;
-					border-radius: 6upx;
-					color: #0091FF;
+					width: 64rpx;
+					height: 32rpx;
+					font-size: 32rpx;
+					font-family: PingFangSC-Medium, PingFang SC;
 					font-weight: 500;
-					// background-color: $base-color;
+					color: #0091FF;
+					line-height: 32rpx;
+
 				}
 			}
-			.mask-content-input{
-				// width: 718upx;//如果textarea的宽带有问题可以把width改为100%试试
-				// width: 100%;
-				// padding: 10upx 16upx 20upx;
-				.textarea {
-					width: 590rpx;
-					height: 28rpx;
-					background: #F8F8F8;
-					border-radius: 4px;
-					padding: 20rpx 16rpx;
-					line-height: 68rpx;
-					// height: 100px;
-					// width: 686upx;//如果textarea的宽带有问题可以把width改为100%试试
-					// width: 100%;
-					// padding: 20rpx 16rpx;
-					// border:2upx solid #d5d5d6;
-					// border-radius: 8upx;
-				}
-			}
+			
 		}
 		&.none{
 			display: none;
