1a3336e22d82a4ae4925326b30268f684d5b05e7
ZZZUTER <805877178@qq.com>
2020-11-16 17:07:18 +0800

    个人中心切换效果更换

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -37,9 +37,34 @@
 					<view class="phone" @click="tell"><image class="phoneImg" src="../../static/images/minephone.svg" mode=""></image></view>
 					<!-- 我的收藏 -->
 					<view class="myCollection" :class="isFixed ? 'fixTabs' : 'noFix'" id="selectcard" >
+						<view class="favBox" @click="change" >
+							<view class="favBT">
+								<view :class="tabCurrent == 0 ?'favText' : 'favText1 '" >
+									收藏
+								</view>
+								<view class="favLine" v-if="tabCurrent == 0">
+								</view>
+							</view>
+							<view :class="tabCurrent == 0 ? 'favNum' : 'favNum1'" :style="{color: favnumcolor.color}" >
+								{{favNum>10000?((favNum-(favNum%1000))/10000+'w'):favNum}}
+							</view>
+						</view>
+						<view class="likeBox" @click="change1">
+							<view class="likeBT">
+								<view :class="tabCurrent == 1 ?'favText' : 'favText1 '">
+									已赞
+								</view>
+								<view class="likeLine" v-if="tabCurrent == 1">
+								</view>
+							</view>
+							<view :class="tabCurrent == 1 ? 'likeNum' : 'likeNum1'" :style="{color: likenumcolor.color}" >
+								{{likeNum>10000?((likeNum-(likeNum%1000))/10000+'w'):likeNum}}
+							</view>
+						</view>
+						
 						<!-- <view :class="isFixed ? 'fixTabs' : 'noFix'" id="selectcard"> -->
 						<!-- <view>收藏</view> -->
-							<v-tabs 
+							<!-- <v-tabs 
 								inactive-color="#909399"
 								lineHeight="24rpx"
 								lineColor="#FFE512"
@@ -53,15 +78,15 @@
 								:current="tabCurrent"
 								@change="tabChange"
 								paddingItem="0 32rpx"
-							></v-tabs>
+							></v-tabs> -->
 							
 						<!-- </view> -->
-						<view :class="tabCurrent == 0 ? 'favNum' : 'favNum1'" :style="{color: favnumcolor.color}" >
+						<!-- <view :class="tabCurrent == 0 ? 'favNum' : 'favNum1'" :style="{color: favnumcolor.color}" >
 							{{favNum>10000?((favNum-(favNum%1000))/10000+'w'):favNum}}
 						</view>
 						<view :class="tabCurrent == 1 ? 'likeNum' : 'likeNum1'" :style="{color: likenumcolor.color}" >
 							{{likeNum>10000?((likeNum-(likeNum%1000))/10000+'w'):likeNum}}
-						</view>
+						</view> -->
 					</view>
 				</view>
 				<!-- 收藏 -->
@@ -247,27 +272,7 @@ export default {
 		getUserMsg() {
 			var that = this;
 
-			// uni.login({
-			// 	provider: 'baidu',
-			// 	success: function(loginRes) {
-			// 		console.log(loginRes.authResult);
-			// 		// 获取用户信息
-			// 		uni.getUserInfo({
-			// 			provider: 'baidu',
-			// 			success: function(infoRes) {
-			// 				console.log('用户昵称为：' + infoRes.userInfo.nickName);
-			// 				var infoRes = infoRes.userInfo;
-			// 				console.log(infoRes.nickName);
-			// 				that.nickName = infoRes.nickName;
-			// 				that.avatarUrl = infoRes.avatarUrl;
-			// 				// that.nickName = infoRes.nickName;
-			// 				// that.avatarUrl = infoRes.avatarUrl;
-			// 				uni.setStorageSync('nickName', infoRes.nickName);
-			// 				uni.setStorageSync('avatarUrl', infoRes.avatarUrl);
-			// 			}
-			// 		});
-			// 	}
-			// });
+			
 
 			uni.getStorage({
 				key: 'Authorization',
@@ -354,18 +359,33 @@ export default {
 				}
 			});
 		},
-		tabChange(index) {
-			this.tabCurrent = index;
-			if(index == 1 ){
-				this.favnumcolor.color = '#909399'
-				this.likenumcolor.color = '#303133'
-				this.downCallback()
-			}else{
-				this.favnumcolor.color = '#303133'
-				this.likenumcolor.color = '#909399'
-				this.downCallback()
-			}
+		
+		// 切换
+		
+		change(){
+			this.tabCurrent = 0
+			this.favnumcolor.color = '#303133'
+			this.likenumcolor.color = '#909399'
+			this.downCallback()
 		},
+		change1(){
+			this.tabCurrent = 1
+			this.favnumcolor.color = '#909399'
+			this.likenumcolor.color = '#303133'
+			this.downCallback()
+		},
+		// tabChange(index) {
+		// 	this.tabCurrent = index;
+		// 	if(index == 1 ){
+		// 		this.favnumcolor.color = '#909399'
+		// 		this.likenumcolor.color = '#303133'
+		// 		this.downCallback()
+		// 	}else{
+		// 		this.favnumcolor.color = '#303133'
+		// 		this.likenumcolor.color = '#909399'
+		// 		this.downCallback()
+		// 	}
+		// },
 		// 跳转文章详情
 		onPageJump(e) {
 			console.log(e);
@@ -667,27 +687,68 @@ export default {
 	position: absolute;
 	top: 360rpx;
 }
+.favBox{
+	display: flex;
+	height: 60rpx;
+	margin-left: 28rpx;
+}
+.favBT{
+	margin-right: 8rpx;
+	text-align: center;
+}
+.favText{
+	width: 72rpx;
+	height: 36rpx;
+	font-size: 36rpx;
+	font-family: PingFangSC-Semibold, PingFang SC;
+	font-weight: 600;
+	color: #303133;
+	line-height: 36rpx;
+	
+}
+.favText1{
+	width: 56rpx;
+	height: 28rpx;
+	font-size: 28rpx;
+	font-family: PingFangSC-Regular, PingFang SC;
+	font-weight: 400;
+	color: #909399;
+	line-height: 28rpx;
+	margin-top: 8rpx;
+}
+.favLine{
+	width: 72rpx;
+	height: 24rpx;
+	background: #FFE512;
+	border-radius: 1px 6px 1px 1px;
+	margin-top: -20rpx;
+}
+.likeBox{
+	display: flex;
+	margin-left: 56rpx;
+	height: 60rpx;
+}
+.likeBT{
+	margin-right: 8rpx;
+	text-align: center;
+}
+.likeText{
+	
+}
+.likeLine{
+	width: 72rpx;
+	height: 24rpx;
+	background: #FFE512;
+	border-radius: 1px 6px 1px 1px;
+	margin-top: -20rpx;
+}
 .fixTabs {
 	position: fixed;
 	top: 126rpx;
-	// padding-left: 10rpx;
-	// padding-top: 15rpx;
-	// left: 0;
 	z-index: 2;
-	// width: 100%;
-	// height:110rpx;
-	// background: #ffffff;
-	// border-bottom: 2rpx solid #eeeeee;
-	// box-shadow: 0px 0px 12rpx 0rpx #eeeeee;
 }
 .noFix {
-	// padding-left: 10rpx;
-	// left: 0;
 	z-index: 1000;
-	// width: 100%;
-	// height: 110rpx;
-	// padding-top: 15rpx;
-	// background: #ffffff;
 }
 .favNum{
 	height: 24rpx;
@@ -697,8 +758,8 @@ export default {
 	color: #303133;
 	line-height: 24rpx;
 	z-index: 11111;
-	margin-top: 26rpx;
-	margin-left: -158rpx;
+	margin-top: 12rpx;
+	// margin-left: -158rpx;
 }
 .favNum1{
 	height: 24rpx;
@@ -708,8 +769,8 @@ export default {
 	color: #303133;
 	line-height: 24rpx;
 	z-index: 11111;
-	margin-top: 26rpx;
-	margin-left: -158rpx;
+	margin-top: 12rpx;
+	// margin-left: -158rpx;
 }
 .likeNum{
 	height: 24rpx;
@@ -719,8 +780,8 @@ export default {
 	color: #303133;
 	line-height: 24rpx;
 	z-index: 11111;
-	margin-top: 26rpx;
-	margin-left: 118rpx;
+	margin-top: 12rpx;
+	// margin-left: 118rpx;
 }
 .likeNum1{
 	height: 24rpx;
@@ -730,8 +791,8 @@ export default {
 	color: #303133;
 	line-height: 24rpx;
 	z-index: 11111;
-	margin-top: 26rpx;
-	margin-left: 118rpx;
+	margin-top: 12rpx;
+	// margin-left: 118rpx;
 }
 
 .noContentItem {
