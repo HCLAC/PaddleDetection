caa5a0c84d3d25abe9e489c652a0f9e5d4b284ed
ZZZUTER <805877178@qq.com>
2021-02-05 10:28:27 +0800

    微信小程序代码优化

--- global.js
+++ global.js
@@ -1,5 +1,5 @@
-// export default 'https://api.lingtuyang.cn/v2'
-export default 'https://devapi.lingtuyang.cn/v2'
+export default 'https://api.lingtuyang.cn/v2'
+// export default 'https://devapi.lingtuyang.cn/v2'
 // export default 'http://192.168.110.69:8199'
 // export default 'http://192.168.110.148:8299/v2'
 // export default 'http://192.168.110.134:8199/v2'
