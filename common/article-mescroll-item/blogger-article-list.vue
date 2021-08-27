3da6fc1ca41cfa1329c27e3cb94befa533e674f3
许伟 <panda_9699@163.com>
2021-08-27 13:48:59 +0800

    【小程序】作者页面显示问题

--- common/article-mescroll-item/blogger-article-list.vue
+++ common/article-mescroll-item/blogger-article-list.vue
@@ -1,5 +1,5 @@
 <template>
-	<view v-show="i === index" style="margin-top: 70%; padding: 0 28rpx;">
+	<view v-show="i === index" style="margin-top: 74%; padding: 0 28rpx;">
 		<mescroll-body :ref="'mescrollRef'+i" @init="mescrollInit" @down="downCallback"  @up="upCallback" :down="downOption" :up="upOption"  >
 			<view class="" v-for="(item, index) in list" :key="index">
 				<view class="contentItem">
