fe362a5e8583f59a4a2fe868436720494c5e1909
许伟 <panda_9699@163.com>
2021-09-08 18:05:44 +0800

    文章加载中图标更换

--- pages_content/article/article.vue
+++ pages_content/article/article.vue
@@ -56,7 +56,7 @@
 				<!-- 内容文章 -->
 				<view class="contentText">
 					<mp-html ref="parse" v-if="articleInfo" style="overflow: hidden;" lazy-load @imgtap="imgTap" @linktap="mpLinktap"
-					 :content="articleInfo.content" loading-img='/static/images/load.png' error-img='/static/images/fail.svg'></mp-html>
+					 :content="articleInfo.content" loading-img='/static/images/load.svg' error-img='/static/images/fail.svg'></mp-html>
 				</view>
 				<view class="tips">
 					<view v-for="item in articleInfo.topics" :key="item.id" @click="toTopic(item.id)">
@@ -92,7 +92,7 @@
 				<!-- 内容文章 -->
 				<view class="contentText">
 					<mp-html ref="parse" v-if="articleInfo" style="overflow: hidden;" lazy-load @imgtap="imgTap" @linktap="mpLinktap"
-					 :content="articleInfo.content" loading-img='/static/images/load.png' error-img='/static/images/fail.svg'></mp-html>
+					 :content="articleInfo.content" loading-img='/static/images/load.svg' error-img='/static/images/fail.svg'></mp-html>
 				</view>
 				<!-- 地址 -->
 				<view class="adress">
